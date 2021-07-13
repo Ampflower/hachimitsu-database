ab02a16b0c7b23918b551673d917fa3444e9b8aa
Ampflower 🌺 <git@ampflower.gay>
2021-07-12 19:47:13 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+// Created 2021-11-07T05:15:41
+
+/**
+ * @author Ampflower
+ * @since ${version}
+ **/
+module gay.ampflower.database {
+    requires java.sql;
+    requires org.objectweb.asm;
+    exports gay.ampflower.hachimitsu.database.api;
+    exports gay.ampflower.hachimitsu.database.api.annotation;
+}
